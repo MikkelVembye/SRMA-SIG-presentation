349600e61a309d7bd0b8ccac49afd14454e58e1d
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2024-11-15 12:17:59 +0100

    151124 1217

--- Example code.R
+++ Example code.R
@@ -1,9 +1,13 @@
+#-------------------------------------------------------------------------------
+# Install and load relevant package
+#-------------------------------------------------------------------------------
+
 # Install from CRAN
 # install.packages("AIscreenR")
 
 # If development version is wanted
 # install.packages("devtools")
-devtools::install_github("MikkelVembye/AIscreenR")
+# devtools::install_github("MikkelVembye/AIscreenR")
 
 # Load relevant package
 
@@ -17,12 +21,19 @@ library(readxl)    # Use if you want to save the screen data to an excel file
 
 options(scipen = 100)
 
-# Ensure you have set the right working directory pointing to the folder in which you have your RIS file(s)
-getwd()
-# Alternatively set working directory
-setwd("C:/Users/B199526/Desktop/GitHub repos/SRMA-SIG-presentation")
+#-------------------------------------------------------------------------------
+# Ensure correct working directory
+#-------------------------------------------------------------------------------
 
+# Ensure the working directory points to the folder containing the RIS file(s)
+getwd()
+# Alternatively set working directory or work in an R project
+#setwd("C:/Users/B199526/Desktop/GitHub repos/SRMA-SIG-presentation")
+
+
+#-------------------------------------------------------------------------------
 # Load and convert RIS file to data frames
+#-------------------------------------------------------------------------------
 
 # Load excluded references
 ris_dat_excl <- revtools::read_bibliography("friends_excl.ris") |> 
@@ -34,7 +45,7 @@ set.seed(20241113)
 excluded_sample <- 
   ris_dat_excl |> 
   as_tibble() |>
-  select(author, eppi_id, title, abstract) |>            # Selecting relevant variables only
+  select(author, eppi_id, title, abstract) |>  # Selecting relevant variables only
   mutate(
     human_code = 0,                                      # Track human decision
     across(c(author, title, abstract), ~ na_if(., "NA")) # Handle missing titles and abstracts
@@ -52,7 +63,7 @@ ris_dat_incl <- revtools::read_bibliography("friends_incl.ris") |>
 included_sample <- 
   ris_dat_incl |> 
   as_tibble() |>
-  select(author, eppi_id, title, abstract) |>            # Selecting relevant variables only
+  select(author, eppi_id, title, abstract) |>  # Selecting relevant variables only
   mutate(
     human_code = 1,                                      # Track human decision
     across(c(author, title, abstract), ~ na_if(., "NA")) # Handle missing titles and abstracts
@@ -66,20 +77,26 @@ test_dat <-
   mutate(studyid = 1:n())
 View(test_dat)
 
+
+#-------------------------------------------------------------------------------
 # Write prompt(s)
-# Can also done in Word, see vignette
+#-------------------------------------------------------------------------------
 
 prompt <- "We are screening studies for a systematic literature review. 
 The topic of the systematic review is the effect of the FRIENDS preventive programme
 on reducing anxiety symptoms in children and adolescents. The FRIENDS programme is a 
 10-session manualised  cognitive behavioural therapy (CBT) programme which can be 
-used as both prevention and  treatment of child and youth anxiety.  The study should 
+used as both prevention and  treatment of child and youth anxiety. The study should 
 focus exclusively on this topic  and we are exclusively searching  for studies with 
-a treatment and a comparison group.  For each study, I would like you to assess:  
+a treatment and a comparison group. For each study, I would like you to assess:  
 1) Is the study about the FRIENDS preventive programme? 
 2) Is the study estimating an effect between a treatment and control/comparison group?"
 
-# Handle your API key 
+
+#-------------------------------------------------------------------------------
+# # Handle your API key 
+#-------------------------------------------------------------------------------
+
 # Get from https://platform.openai.com/settings/organization/api-keys 
 set_api_key()
 
@@ -90,16 +107,22 @@ set_api_key()
 # To see how the AIscreenR function handle your key
 ?tabscreen_gpt
 
+#-------------------------------------------------------------------------------
+# Inspect model access
+#-------------------------------------------------------------------------------
+
 # Get you info about your model access and rate limits
 rate_limit <- AIscreenR::rate_limits_per_minute() 
 rate_limit
 
-# See acroos all available models
+# See across all available models
 rlpm_all_models <- rate_limits_per_minute(model = model_prizes$model)
 View(rlpm_all_models)
 
 
-# Run first screening ----------
+#-------------------------------------------------------------------------------
+# Run first screening
+#-------------------------------------------------------------------------------
 
 # Set parallel plan
 plan(multisession)
@@ -108,41 +131,42 @@ result_obj <-
   AIscreenR::tabscreen_gpt(
     data = test_dat,                       # The data
     prompt = prompt,                       # The prompt(s) - can take multiple inputs
-    studyid = eppi_id,                     # Variable name containing the study IDs
+    studyid = studyid,                     # Variable name containing the study IDs
     title = title,                         # Variable name containing the titles
     abstract = abstract,                   # Variable name containing abstracts
     model = "gpt-4o-mini",                 # Model - can take multiple inputs
     reps = 1,                              # The number of screenings you want to run
     rpm = rate_limit$requests_per_minute   # Requests per minute
-    
-  ); result_obj
+  )
 
 # Back the sequential plan
 plan(sequential)
 
+result_obj
+
 result_obj$answer_data |> View()
 
 #saveRDS(result_obj, file = "res_obj_4o-mini.rds")
+# Worst case
 #result_obj2 <- readRDS("res_obj_4o-mini.rds")
 
-
-# Analyze performance
+#-------------------------------------------------------------------------------
+# Analyze screening performance
+#-------------------------------------------------------------------------------
 performance <- 
   result_obj |> 
   AIscreenR::screen_analyzer(
-    # Name variable with human decisions
-    human_decision = human_code,
-    # State whether only key results should be printed
-    key_result = TRUE # Default
+    human_decision = human_code, # Name variable with human decisions
+    key_result = TRUE # # State whether only key results should be printed. Default is true
   )
 
 performance 
 
-# Use multiple screenings
-plan(multisession)
+#-------------------------------------------------------------------------------
+# Using multiple screening
+#-------------------------------------------------------------------------------
 
-# To track time
-#start <- Sys.time()
+plan(multisession)
 
 result_obj_reps10 <-
   tabscreen_gpt(
@@ -155,15 +179,14 @@ result_obj_reps10 <-
     reps = 10,
     rpm = rate_limit$requests_per_minute
   )
-#end <- Sys.time()
-#end - start 
-
-result_obj_reps10
 
 plan(sequential)
 
 #saveRDS(result_obj_reps10, file = "res_obj_4o-mini_reps10.rds")
-#result_obj_reps10 <- readRDS("res_obj_4o-mini_reps10.rds")
+# I have cheated from home
+result_obj_reps10 <- readRDS("res_obj_4o-mini_reps10.rds")
+result_obj_reps10
+
 
 performance10 <- 
   result_obj_reps10 |> screen_analyzer()
@@ -174,9 +197,12 @@ performance10
 performance_dist <- attr(performance10, "p_incl_data")
 performance_dist |> View()
 
-# If time allows for it
+
+#-------------------------------------------------------------------------------
 # Check disagreement between human and GPT
 # Remember also to check studies included by GPT and excluded by humans. 
+#-------------------------------------------------------------------------------
+
 disagree_dat <- 
   result_obj_reps10$answer_data_aggregated |> 
   filter(human_code == 1, incl_p == 0) 
@@ -191,7 +217,7 @@ dis_res <-
     title = title,
     abstract = abstract,
     model = "gpt-4o-mini",
-    decision_description = TRUE,
+    decision_description = TRUE, # Set true to get detailed responses
     reps = 10, 
     rpm = rate_limit$requests_per_minute
   )
@@ -203,10 +229,15 @@ plan(sequential)
 # Worst case
 #dis_res <- readRDS("disagreement_description_res_obj_4o-mini_10reps.rds")
 
-dis_res$answer_data_aggregated |> select(author, abstract, final_decision_gpt, longest_answer) |> View()
+dis_res$answer_data_aggregated |> 
+  select(author, abstract, final_decision_gpt, longest_answer) |> 
+  View()
 
+#-------------------------------------------------------------------------------
+# Report and comment on disaggreements
+#-------------------------------------------------------------------------------
 
-# Report function - vil indlægge senere i AIscreenR --------
+# Report function - will eventually be added to AIscreenR --------
 report <- 
   function(
     data, studyid, title, abstract, answer, 
@@ -260,7 +291,7 @@ base::cat(print_test)
     
   }
 
-# Get a report with screening disaggreements 
+# Get a report with screening disagreements 
 report(
   data = dis_res$answer_data_aggregated, 
   studyid = studyid, 
@@ -270,8 +301,12 @@ report(
   format = "word_document"
 )
 
-# Approximate price
-# Approximate price full screening (2862 refs)
+
+#-------------------------------------------------------------------------------
+# Approximate price 
+#-------------------------------------------------------------------------------
+
+# Approximate price of full screening (2862 refs)
 all_dat <- bind_rows(ris_dat_excl, ris_dat_incl) |> as_tibble() 
 nrow(all_dat)
 
